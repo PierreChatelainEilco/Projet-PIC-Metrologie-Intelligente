82a214f0480d68eb057ab8ec2af24edf3d284127
Pierre Chatelain <pierre.chatelain.elv@eilco-ulco.fr>
2018-12-04 00:24:56 +0100

    Recharger le projet sans recompiler le package

---
+++ chargerLeProjetSansCompilation.r
@@ -0,0 +1,14 @@
+# Charger les bibliotheques
+paquets=c("tcltk","tcltk2","tkrplot", "HMM", "clValid", "class", "cluster","FactoMineR", "corrplot", "chron");
+for(paquet in paquets)
+{
+  library(paquet,character.only = T);
+}
+
+# Charger les fichiers R
+repertoireSource="C:\\Users\\Toto\\interfaceIfremer\\uHMM\\R";
+fichiers = list.files(path=repertoireSource,pattern=".r",full.names = T);
+for(fichier in fichiers)
+{
+  source(fichier);
+}
