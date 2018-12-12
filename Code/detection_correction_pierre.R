acf43aeed0c652e10e0b4dc1de87157f597b2f33
Pierre Chatelain <pierre.chatelain.elv@eilco-ulco.fr>
2018-12-12 10:44:24 +0100

    Mise a jour du code des outliers

--- Code/detection_correction_pierre.R
+++ Code/detection_correction_pierre.R
@@ -1,5 +1,5 @@
 ### import data test
-data <- read.csv(file="")
+data <- read.csv(file="C:\\Users\\Remi\\Desktop\\Eil\\ING3\\PIC\\Notebook\\test.csv")
 
 ### Calcul de toutes les valeurs de la boite a moustache stocker dans une variable table
 infodata<-c()
@@ -20,18 +20,19 @@ for(i in seq(1,dim(dataCorrige)[2]))
 }
 
 ### Vecteur de boolean inf 1e quartile
+
 infodata <- cbind(infodata,IQR = infodata[,5]-infodata[,2])
 ext <- c()
 for(i in seq(1,dim(infodata)[1]))
 {
-  ext <- c(ext,max(infodata[i,1],infodata[i,2]-infodata[i,"IQR"]))
+  ext <- c(ext,max(infodata[i,1],infodata[i,2]-1.5*infodata[i,"IQR"]))
 }
 infodata <- cbind(infodata,ext)
 colnames(infodata)[length(colnames(infodata))] <- "Ext. inf."
 ext <- c()
 for(i in seq(1,dim(infodata)[1]))
 {
-  ext <- c(ext,min(infodata[i,6],infodata[i,5]+infodata[i,"IQR"]))
+  ext <- c(ext,min(infodata[i,6],infodata[i,5]+1.5*infodata[i,"IQR"]))
 }
 infodata <- cbind(infodata,ext)
 colnames(infodata)[length(colnames(infodata))] <- "Ext. sup."
@@ -42,9 +43,9 @@ positions = c()
 for(i in seq(1,dim(dataCorrige)[2]))
 {
   v <- dataCorrige[i]
-  position <- (v<infodata[i,"Ext. inf."]) + (v>infodata[i,"Ext. sup."])
+  position <- -(v<infodata[i,"Ext. inf."]) + (v>infodata[i,"Ext. sup."])
   positions <- cbind(positions,position)
-  nb <- sum(position)
+  nb <- sum(position!=0)
   outliers <- c(outliers,nb)
 }
 infodata<- cbind(infodata,Outliers=outliers)
